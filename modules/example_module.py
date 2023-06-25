966edbbdf24fc945755062247fff2664cf040c13
Stanton-e <stanton.e@gmail.com>
2023-06-25 17:11:38 -0500

    Update example_module.py

--- modules/example_module.py
+++ modules/example_module.py
@@ -1,15 +1,16 @@
-import os
-import sys
-
+#import os
+#import sys
+"for this to work you'd have to remove # and the ' " '' """in be begining
+"""
 """
 This is a blank template.
 Add your code within the triple quotes.
 """
 
 # Example code: Print "hello world"
-print('hello world')
+#print('hello world')
 
 # Example code: Wait for a key press
 # Note: Implementing a key press wait may require additional libraries or platform-specific code.
 #       This is just a placeholder for demonstration purposes.
-wait = input("Press any key to continue...")
+#wait = input("Press any key to continue...")
