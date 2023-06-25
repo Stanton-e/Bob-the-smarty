6f5cb12c85994bbba5b6e035fb8f604766dcf1ec
Stanton-e <stanton.e@gmail.com>
2023-06-25 17:08:02 -0500

    Create example_module.py
    being all fancy

---
+++ modules/example_module.py
@@ -0,0 +1,15 @@
+import os
+import sys
+
+"""
+This is a blank template.
+Add your code within the triple quotes.
+"""
+
+# Example code: Print "hello world"
+print('hello world')
+
+# Example code: Wait for a key press
+# Note: Implementing a key press wait may require additional libraries or platform-specific code.
+#       This is just a placeholder for demonstration purposes.
+wait = input("Press any key to continue...")
